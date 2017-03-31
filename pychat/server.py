40a71dbe7b81e879fcb26aa0df00f672bef20a57
sujeevan07 <jsujeevan07@gmail.com>
2017-03-31 15:48:04 +0200

    Emplement the private messages

--- pychat/server.py
+++ pychat/server.py
@@ -8,14 +8,13 @@ from datetime import datetime
 
 from flask import request, render_template, Flask, jsonify
 
-from sys import argv
-
 app = Flask(__name__)
 
 
 # holds all messages. A message is a dict with keys 'nickname', 'message', and
 # 'time'
 message_list = []
+
 
 @app.route('/')
 def index():
@@ -47,7 +46,21 @@ def get_all_messages(nickname):
 
     # note that jsonify() automatically sets correct http header
     # ("Content-type: application/json" etc.)
-    return jsonify(message_list)
+    tem_mesg=[]
+    for message in message_list:
+        # print(message)
+        if message["only_for"] !=None and message["only_for"]==nickname :
+            tem_mesg.append(message)
+
+        if message["only_for"] ==None:
+            tem_mesg.append(message)
+
+        if message["nickname"]==nickname and message["only_for"] !=None :
+            tem_mesg.append(message)
+    print(tem_mesg)
+
+
+    return jsonify(tem_mesg)
 
 
 @app.route('/chat/post', methods=('POST', ))
@@ -70,18 +83,32 @@ def post_message():
     # "somenickname", for example)
 
     # Check if json data has "nickname" and "message" attributes.
+
+
     # If data contains other attributes, we ignore them.
+    nick_name = None
+    if data["message"].startswith('@'):
+        nick_name = data["message"].split(" ")[0][1:]
+
+        print(nick_name)
+
     try:
+
         message_list.append({
             "message": data["message"],
             "nickname": data["nickname"],
             # we add a date/time to the message
             "time": datetime.utcnow(),
+            "only_for":nick_name
         })
+
+
     except KeyError:
         # http status code 400 means "Bad Request"
         return ("Received invalid json data! Must contain attributes "
                 "'message' and 'nickname'.", 400)
+
+
 
     # response has http status code 200 (meaning "successful")
     return 'Message posted!', 200
@@ -90,4 +117,4 @@ def post_message():
 if __name__ == "__main__":
     # this will start the integrated flask web server (meant for development,
     # not for production use)
-    app.run(debug=True, host="0.0.0.0", port=int(argv[1]))
+    app.run(debug=True, host="0.0.0.0", port=8080)
